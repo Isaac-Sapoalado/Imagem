e1ac8dcfbf9f5836810abe5b773de326f9730589
ISAAC <sapoalado070@gmail.com>
2024-07-16 17:25:27 -0300

    testando cookie.2

--- pyutils-front/auth/cad.js
+++ pyutils-front/auth/cad.js
@@ -81,12 +81,12 @@ function setCookie(nome,valor,dias=null) {
         date.setTime(date.getTime() + (dias*24*60*60*1000));
         validade = ", expires=" + date.toUTCString();
     }
-    document.cookie = nome + "=" + (valor || "")  + validade + ", path=/";
+    document.cookie = nome + "=" + (valor || "")  + validade + "; path=/";
 }
 
 function getCookie(nome) {
     var nomeCookie = nome + "=";
-    var ca = document.cookie.split(',');
+    var ca = document.cookie.split(';');
     for(var i=0;i < ca.length;i++) {
         var c = ca[i];
         while (c.charAt(0)==' ') c = c.substring(1,c.length);
@@ -96,5 +96,5 @@ function getCookie(nome) {
 }
 
 function eraseCookie(nome) {   
-    document.cookie = nome +'=, Path=/, Expires=Thu, 01 Jan 1970 00:00:01 GMT,';
+    document.cookie = nome +'=; Path=/; Expires=Thu; 01 Jan 1970 00:00:01 GMT;';
 }
